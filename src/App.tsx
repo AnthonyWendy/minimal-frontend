28d6f9f1e96fddad9b32705736674ae49d2a5b5c
AnthonyWendy <anthonywendyantunesdelima@gmail.com>
2024-10-29 00:19:32 -0300

    feat: Adicionar tema claro e o provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
+import { ThemeProvider } from "@emotion/react";
+import { light } from "@mui/material/styles/createPalette";
+import { LightTheme } from "./shared/themes";
 
 export const App = () => {
   return (
-    <div className="App">
-anthony
-    </div>
+    <ThemeProvider theme={LightTheme}>
+      <BrowserRouter>
+        <AppRoutes />
+      </BrowserRouter>
+    </ThemeProvider>
   );
-}
+};

aefe2a9179f140a6aeeeec85e0d03e92390aaae7
AnthonyWendyAM <anthony.lima@amplimed.com.br>
2024-11-13 16:17:56 -0300

    Feat: trocar de tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
-import { ThemeProvider } from "@emotion/react";
-import { light } from "@mui/material/styles/createPalette";
-import { LightTheme } from "./shared/themes";
+import { AppThemeProvider } from "./shared/contexts";
 
 export const App = () => {
   return (
-    <ThemeProvider theme={LightTheme}>
+    <AppThemeProvider>
       <BrowserRouter>
         <AppRoutes />
       </BrowserRouter>
-    </ThemeProvider>
+    </AppThemeProvider>
   );
 };

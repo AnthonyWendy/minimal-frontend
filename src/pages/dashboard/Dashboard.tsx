3fe21d82dbbdd46190f8eb913e82de94c63da00d
AnthonyWendy <anthonywendyantunesdelima@gmail.com>
2025-08-25 19:33:39 -0300

    apagando tudo

--- src/pages/dashboard/Dashboard.tsx
+++
@@ -1,9 +0,0 @@
-import { LayoutBaseDePagina } from "../../shared/layouts/LayoutBaseDePagina";
-
-export const Dashboard = () => {
-    return (
-        <LayoutBaseDePagina titulo="Dashboard" barraDeFerramentas={<> barrade de ferramentas</>}>
-            dashboard
-        </LayoutBaseDePagina>
-    );
-};
